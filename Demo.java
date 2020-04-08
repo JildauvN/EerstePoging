3aa91546ab66f9d396be18d71c43526b971331d2
JildauvN <jildau94@hotmail.com>
2020-04-08 16:19:03 +0200

    Tekst hier

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ class Demo{
 	public static void main(String[] args){
 		System.out.println("Hij doet het!");
 		System.out.println("Hij doet het! 2e");
-		System.out.println("Hij doet het! Via VI 3e");
+		System.out.println("Hij doet het! 3e");
 	}
 }
